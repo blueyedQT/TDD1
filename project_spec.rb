6f4e42664568d343386c08837602e80624c28b84
Katrina <blueyed_qt@hotmail.com>
2015-11-07 15:47:13 -0800

    Added test to check with initializer, and updated previous tests

--- project_spec.rb
+++ project_spec.rb
@@ -1,17 +1,21 @@
 require_relative "project"
 RSpec.describe Project do 
-	# message describing behavior - we will see this if the test fails
-	it "should have a getter and a setter for the name attribute" do
-		# the code that will run to check if the behavior works
-		project = Project.new
-		project.name = "Project Name"
-		# what the result should be in order for the test to pass
-		expect(project.name).to eq("Project Name")
+	it "has a getter and a setter for the name attribute" do
+		# pass the name and the description aguments when instantiating
+		project = Project.new("Project Name", "I am a project")
+		# accessing again will change the name
+		project.name = "Changed Name"
+		# check that the name has changed
+		expect(project.name).to eq("Changed Name")
 	end
-	# add the test below to your spec file
 	it "has a getter and a setter for description attribute" do
-		project = Project.new
-		project.description = "I am a project"
+		project = Project.new("Project Name", "I am a project")
+		project.description = "Changed Description"
+		expect(project.description).to eq("Changed Description")
+	end
+	it "has an initializer with arguments name and description" do 
+		project = Project.new("Project Name", "I am a project")
+		expect(project.name).to eq("Project Name")
 		expect(project.description).to eq("I am a project")
 	end
 end
