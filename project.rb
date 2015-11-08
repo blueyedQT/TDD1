1500140244f5f139434ecd63176ee6c2c3156c43
Katrina <blueyed_qt@hotmail.com>
2015-11-07 17:33:47 -0800

    Updated test to include a test for new method 'elevator_pitch'

--- project.rb
+++ project.rb
@@ -1,8 +1,11 @@
 class Project
 	attr_accessor :name, :description
-
 	def initialize(name, description)
 		@name = name
 		@description = description
+	end
+
+	def elevator_pitch
+		return @name+" "+@description
 	end
 end
--- project_spec.rb
+++ project_spec.rb
@@ -1,21 +1,25 @@
 require_relative "project"
 RSpec.describe Project do 
 	it "has a getter and a setter for the name attribute" do
-		# pass the name and the description aguments when instantiating
 		project = Project.new("Project Name", "I am a project")
-		# accessing again will change the name
 		project.name = "Changed Name"
-		# check that the name has changed
 		expect(project.name).to eq("Changed Name")
 	end
 	it "has a getter and a setter for description attribute" do
 		project = Project.new("Project Name", "I am a project")
 		project.description = "Changed Description"
 		expect(project.description).to eq("Changed Description")
-	end
+	end	
 	it "has an initializer with arguments name and description" do 
 		project = Project.new("Project Name", "I am a project")
 		expect(project.name).to eq("Project Name")
 		expect(project.description).to eq("I am a project")
+	end
+	it "has a method elevator_pitch to explain name and description" do
+    	project1 = Project.new("Project 1", "description 1")
+	    project2 = Project.new("Project 2", "description 2")
+	    # when the new method is invoked, the output should be...
+	    expect(project1.elevator_pitch).to eq("Project 1 description 1")
+	    expect(project2.elevator_pitch).to eq("Project 2 description 2")
 	end
 end
